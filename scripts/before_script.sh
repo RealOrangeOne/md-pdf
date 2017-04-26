13006922d70acea80316497563e3fc3bfd055d6e
Jake Howard <git@theorangeone.net>
2017-04-26 14:54:20 +0100

    Actually run the application

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 sudo apt-get install -y openssl build-essential xorg libssl-dev
 wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
 tar -xJf wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
