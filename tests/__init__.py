8ce8a6f9407477cf023f2857ec1fa51ad07659ac
Jake Howard <git@theorangeone.net>
2017-05-27 16:43:23 +0100

    Delete templates after test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,20 @@
 from unittest import TestCase
+import os
+from md_pdf.consts import TEMPLATES_DIR
 
 
 class BaseTestCase(TestCase):
-    pass
+    def deleteTemplates(self):
+        for template in [
+            'header.html',
+            'footer.html',
+            'cover.html',
+            'toc-xml',
+        ]:
+            try:
+                os.remove(os.path.join(TEMPLATES_DIR, template))
+            except OSError:
+                pass
+
+    def tearDown(self):
+        self.deleteTemplates()
